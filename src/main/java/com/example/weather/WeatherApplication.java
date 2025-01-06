0348389498a251980ac577d0939390cb8777f8b9
ancao99 <caonuhongan@gmail.com>
2025-01-06 13:04:31 -0500

    a

--- src/main/java/com/example/weather/WeatherApplication.java
+++ src/main/java/com/example/weather/WeatherApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class WeatherApplication {
     public static void main(String[] args) {
         SpringApplication.run(WeatherApplication.class, args);
